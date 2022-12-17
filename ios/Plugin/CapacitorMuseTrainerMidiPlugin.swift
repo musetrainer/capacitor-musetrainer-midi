3166ad4b1f16bfceaa994fd14e5fcb2f15971c0f
manhtai <vo@manhtai.com>
2022-12-17 10:20:12 +0700

    Support initial connect

--- ios/Plugin/CapacitorMuseTrainerMidiPlugin.swift
+++ ios/Plugin/CapacitorMuseTrainerMidiPlugin.swift
@@ -11,87 +11,114 @@ public class CapacitorMuseTrainerMidiPlugin: CAPPlugin {
         !dv.isVirtual && dv.entities.count > 0 && !(dv.manufacturer ?? "").isEmpty
     }
     
-    override public func load() {
-        midiDevicesObserver = deviceManager.observe(\.availableDevices) { (dm, _) in
-            // Devices change
-            let d = Dictionary.init(uniqueKeysWithValues: dm.availableDevices
-                .filter(self.validDevice)
-                .enumerated()
-                .map({ (String($0), $1.manufacturer ?? "") }))
-            self.notifyListeners("deviceChange", data: d)
-            
-            // Listen to MIDI events from all sources
-            for device in self.deviceManager.availableDevices.filter(self.validDevice) {
-                for entity in device.entities {
-                    for source in entity.sources {
-                        do {
-                            try self.deviceManager.connectInput(source, eventHandler: { (_, cmds) in
-                                for cmd in cmds {
-                                    var cmdType: String
-                                    switch cmd.commandType {
-                                    case .noteOff:
-                                        cmdType = "noteOff"
-                                    case .noteOn:
-                                        cmdType = "noteOn"
-                                    case .polyphonicKeyPressure:
-                                        cmdType = "polyphonicKeyPressure"
-                                    case .controlChange:
-                                        cmdType = "controlChage"
-                                    case .programChange:
-                                        cmdType = "programChange"
-                                    case .channelPressure:
-                                        cmdType = "channelPressure"
-                                    case .pitchWheelChange:
-                                        cmdType = "pitchWheelChange"
-                                    case .systemMessage:
-                                        cmdType = "systemMessage"
-                                    case .systemExclusive:
-                                        cmdType = "systemExclusive"
-                                    case .systemTimecodeQuarterFrame:
-                                        cmdType = "systemTimecodeQuarterFrame"
-                                    case .systemSongPositionPointer:
-                                        cmdType = "systemSongPositionPointer"
-                                    case .systemSongSelect:
-                                        cmdType = "systemSongSelect"
-                                    case .systemTuneRequest:
-                                        cmdType = "systemTuneRequest"
-                                    case .systemTimingClock:
-                                        cmdType = "systemTimingClock"
-                                    case .systemStartSequence:
-                                        cmdType = "systemStartSequence"
-                                    case .systemContinueSequence:
-                                        cmdType = "systemContinueSequence"
-                                    case .systemStopSequence:
-                                        cmdType = "systemStopSequence"
-                                    case .systemKeepAlive:
-                                        cmdType = "systemKeepAlive"
-                                    @unknown default:
-                                        cmdType = "unknown"
-                                    }
-                                    
-                                    self.notifyListeners("commandReceive", data: [
-                                        "type": cmdType,
-                                        "dataByte1": cmd.dataByte1,
-                                        "dataByte2": cmd.dataByte2
-                                    ])
+    func alreadyConnected(_ sources: [MIKMIDISourceEndpoint], _ source: MIKMIDISourceEndpoint) -> Bool {
+        for src in sources {
+            if src == source {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func listenForDevices(_ dm: MIKMIDIDeviceManager) {
+        let d = Dictionary.init(uniqueKeysWithValues: dm.availableDevices
+            .filter(self.validDevice)
+            .enumerated()
+            .map({ (String($0), $1.manufacturer ?? "") }))
+        self.notifyListeners("deviceChange", data: d)
+    }
+    
+    func listenForCommands(_ dm: MIKMIDIDeviceManager) {
+        for device in dm.availableDevices.filter(self.validDevice) {
+            for entity in device.entities {
+                for source in entity.sources {
+                    if self.alreadyConnected(dm.connectedInputSources, source) {
+                        continue
+                    }
+                    
+                    do {
+                        try self.deviceManager.connectInput(source, eventHandler: { (_, cmds) in
+                            for cmd in cmds {
+                                var cmdType: String
+                                switch cmd.commandType {
+                                case .noteOff:
+                                    cmdType = "noteOff"
+                                case .noteOn:
+                                    cmdType = "noteOn"
+                                case .polyphonicKeyPressure:
+                                    cmdType = "polyphonicKeyPressure"
+                                case .controlChange:
+                                    cmdType = "controlChage"
+                                case .programChange:
+                                    cmdType = "programChange"
+                                case .channelPressure:
+                                    cmdType = "channelPressure"
+                                case .pitchWheelChange:
+                                    cmdType = "pitchWheelChange"
+                                case .systemMessage:
+                                    cmdType = "systemMessage"
+                                case .systemExclusive:
+                                    cmdType = "systemExclusive"
+                                case .systemTimecodeQuarterFrame:
+                                    cmdType = "systemTimecodeQuarterFrame"
+                                case .systemSongPositionPointer:
+                                    cmdType = "systemSongPositionPointer"
+                                case .systemSongSelect:
+                                    cmdType = "systemSongSelect"
+                                case .systemTuneRequest:
+                                    cmdType = "systemTuneRequest"
+                                case .systemTimingClock:
+                                    cmdType = "systemTimingClock"
+                                case .systemStartSequence:
+                                    cmdType = "systemStartSequence"
+                                case .systemContinueSequence:
+                                    cmdType = "systemContinueSequence"
+                                case .systemStopSequence:
+                                    cmdType = "systemStopSequence"
+                                case .systemKeepAlive:
+                                    cmdType = "systemKeepAlive"
+                                @unknown default:
+                                    cmdType = "unknown"
                                 }
-                            })
-                        } catch {
-                            self.notifyListeners("connectError", data: [source.displayName ?? "Unknown": String(describing: error)])
-                        }
+                                
+                                self.notifyListeners("commandReceive", data: [
+                                    "type": cmdType,
+                                    "dataByte1": cmd.dataByte1,
+                                    "dataByte2": cmd.dataByte2
+                                ])
+                            }
+                        })
+                    } catch {
+                        self.notifyListeners("connectError", data: [source.displayName ?? "Unknown": String(describing: error)])
                     }
                 }
             }
         }
     }
     
+    override public func load() {
+        midiDevicesObserver = deviceManager.observe(\.availableDevices) { (dm, _) in
+            // Listen for devices change
+            self.listenForDevices(dm)
+            
+            // Listen for MIDI events from all sources
+            self.listenForCommands(dm)
+        }
+    }
+    
     @objc func listDevices(_ call: CAPPluginCall) {
-        call.resolve([
-            "devices": MIKMIDIDeviceManager.shared.availableDevices
+        // Attach input sources
+        if deviceManager.connectedInputSources.count == 0 {
+            self.listenForDevices(deviceManager)
+        }
+        
+        call.resolve(
+            Dictionary.init(uniqueKeysWithValues: deviceManager.availableDevices
                 .filter(self.validDevice)
                 .enumerated()
                 .map({ (String($0), $1.manufacturer ?? "") })
-        ])
+            )
+        )
     }
     
     @objc func sendCommand(_ call: CAPPluginCall) {
