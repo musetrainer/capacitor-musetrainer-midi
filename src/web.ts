c32cb5838d59148742726508e5d12e0c766d9f2e
manhtai <vo@manhtai.com>
2023-01-05 20:55:14 +0700

    Ignore ban-ts-comment

--- src/web.ts
+++ src/web.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import type { ListenerCallback, PluginListenerHandle } from '@capacitor/core';
 import { WebPlugin } from '@capacitor/core';
 
