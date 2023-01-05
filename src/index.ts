b829878ef59d7e5f3029fd963c1b10cf1977676d
manhtai <vo@manhtai.com>
2023-01-05 20:33:25 +0700

    Support webmidi

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import type { CapacitorMuseTrainerMidiPlugin } from './definitions';
 
 const CapacitorMuseTrainerMidi = registerPlugin<CapacitorMuseTrainerMidiPlugin>(
   'CapacitorMuseTrainerMidi',
+  {
+    web: () => import('./web').then(m => new m.CapacitorMuseTrainerMidiWeb()),
+  },
 );
 
 export * from './definitions';
